ce4c6c125774063691459e2cf00427e379b16e1e
ClauuGithub <164614753+ClauuGithub@users.noreply.github.com>
2025-11-29 14:49:10 +0100

    Fuente implementada

--- PruebaJER/src/entities/Button.js
+++ PruebaJER/src/entities/Button.js
@@ -1,8 +1,16 @@
 export class Button {
     constructor(scene, x, y, texture, text, style, callback) {
         this.scene = scene;
+
+        this.style = {
+            fontFamily: 'ButtonsFont', 
+            fontSize: '24px',            
+            color: '#00ff00'                        
+        };
+
         this.image = scene.add.image(0, 0, texture);
         this.text = scene.add.text(0, 0, text, style).setOrigin(0.5);
+
         this.container = scene.add.container(x, y, [this.image, this.text]);
         this.container.setSize(this.image.width, this.image.height);
         this.container.setInteractive({ useHandCursor: true })
