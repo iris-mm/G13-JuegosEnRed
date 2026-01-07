e4092229bdc5602cd1d728a63d1275d30a663077
ClauuGithub <164614753+ClauuGithub@users.noreply.github.com>
2026-01-07 20:48:42 +0100

    coge power up

--- Game/src/client/game/items/OnlineSpeedPowerUp.js
+++ Game/src/client/game/items/OnlineSpeedPowerUp.js
@@ -4,31 +4,44 @@ export class OnlineSpeedPowerUp extends Entity {
     constructor(x, y, scale, scene, id) {
         super(x, y, scale, 'speed_powerup', scene, true);
 
-        this.id = id;
         this.scene = scene;
+        this.id = id;
         this.active = true;
+
+        this.gameObject.body.setAllowGravity(false);
+        this.gameObject.body.setSize(
+            this.gameObject.width * scale,
+            this.gameObject.height * scale
+        );
+        this.gameObject.body.setOffset(
+            (this.gameObject.width - this.gameObject.body.width) / 2,
+            (this.gameObject.height - this.gameObject.body.height) / 2
+        );
+
+        this.gameObject.body.setEnable(true);
+        this.gameObject.setVisible(true);
+
     }
 
     setupOverlap(localPlayer, scene) {
         scene.physics.add.overlap(
             this.gameObject,
             localPlayer.gameObject,
-            () => this.collect(localPlayer),
+            () => this.onCollected(),
             null,
             this
         );
     }
 
-    collect(player) {
+    onCollected() {
         if (!this.active) return;
 
         this.active = false;
 
-        // Avisar al servidor
+        // Avisar al servidor. NO aplicar efecto aquí
         this.scene.send({
             type: 'POWERUP_COLLECTED',
-            powerUpId: this.id,
-            player: this.scene.playerRole
+            powerUpId: this.id
         });
     }
 
@@ -43,5 +56,7 @@ export class OnlineSpeedPowerUp extends Entity {
         this.gameObject.setVisible(true);
         this.gameObject.body.enable = true;
         this.active = true;
+
+        this.gameObject.body.updateFromGameObject();
     }
 }
